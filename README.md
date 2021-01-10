fff6a46bc850df29a5e697f44a918be49723c14c
casemattersAles76 <44009231+alesToniolo76@users.noreply.github.com>
2021-01-10 11:58:38 -0500

    Project Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Quote REST API
+
+## Welcome
+This is a REST API, specifically for popular quotes, that I built as a learning practice.
+It allows for all CRUD operations and uses a simple JSON data store for these quotes.
+By making a get, post, put, and delete request to the api endpoint, you can manipulate and view
+the datastore of quotes.
+
+## Getting Started
+To use this API, you can simply clone the repository, `npm install` to install all project
+dependencies, and then start up a local server with `npm start`.
+
+## About
+The project is fully backend, written in JavaScript. It uses node.js to be backend, and
+utilizes the [express](https://github.com/expressjs/express) library for all routing and functionality.
+I tested the routes using the Postman software. In the datastore are some random inspirational quotes.
+In addition, there is a random api endpoint for retreiving a random quote out of the data store.
+
---
+++ routes.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const router = express.Router();
+const records = require('./records');
+
+function asyncHandler(cb){
+    return async (req, res, next)=>{
+      try {
+        await cb(req,res, next);
+      } catch(err){
+        next(err);
+      }
+    };
+  }
+
+  router.get('/quotes', async (req, res)=>{
+    try {
+      const quotes = await records.getQuotes();
+      res.json(quotes);
+    } catch(err) {
+      res.json({message: err.message});
+    }
+  });
+  
+  router.get('/quotes/:id', async (req, res)=>{
+    try{
+      const quote = await records.getQuote(req.params.id);
+      if (quote){
+        res.json(quote);
+      } else {
+        res.status(404).json({message: "Quote not found"});
+      }
+    } catch(err){
+      res.status(500).json({message: err.message});
+    }
+  
+  });
+  
+  router.get('/quotes/quote/random', asyncHandler(async(req, res)=>{
+    const random = await records.getRandomQuote();
+    res.json(random);
+  }));
+
+  router.post('/quotes', asyncHandler(async(req, res)=>{
+    if (req.body.author && req.body.quote) {
+      const quote = await records.createQuote({
+        quote: req.body.quote,
+        author: req.body.author
+      });
+      res.status(201).json(quote);
+    } else {
+      res.status(400).json({message: "Quote and author not provided"});
+    }
+  }));
+  
+  router.put('/quotes/:id', asyncHandler(async(req, res)=>{
+        const quote = await records.getQuote(req.params.id);
+        if (quote) {
+          quote.quote = req.body.quote;
+          quote.author = req.body.author;
+  
+          await records.updateQuote(quote);
+          res.status(204).end();
+        } else {
+          res.status(404).json({message: "Quote not found"});
+        }
+        res.status(500).json({message: err.message});
+  }));
+  
+  router.delete('/quotes/:id', async (req, res, next)=>{
+    try {
+      const quote = await records.getQuote(req.params.id);
+      await records.deleteQuote(quote);
+      res.status(204).end();
+    } catch (err) {
+      next(err);
+    }
+  });
+
+module.exports = router;
